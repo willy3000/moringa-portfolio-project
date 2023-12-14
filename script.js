1515ae84f4136bce5d0e2a34e0b176146accbb01
Jillo Wario <willywario0@gmail.com>
2023-12-14 12:58:03 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const scrollto = (section) => {
+//     console.log('working')
+//     document.querySelector(`#${section}`).scrollIntoView({
+//         behavior: 'smooth'
+//     });
+// }
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+    console.log("scrolled added");
+  } else {
+    header.classList.remove("scrolled");
+    console.log("scrolled removed");
+  }
+});
